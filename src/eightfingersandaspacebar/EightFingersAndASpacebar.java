6635958dee6daa012e5ee4ec6c382aeea6a8beda
ShirDav18 <ShirDav18@SHS-PLAB-67026.shs.isd.issaquah.wednet.edu>
2017-06-07 11:32:31 -0700

    created game
    make initial frame and couple pages (home and typing)

---
+++ src/eightfingersandaspacebar/EightFingersAndASpacebar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eightfingersandaspacebar;
+
+/**
+ *
+ * @author shirdav18
+ */
+public class EightFingersAndASpacebar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
